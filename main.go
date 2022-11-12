8bf7a7535b8eb0ea9b10a6cec4bb00631067964f
Thiago Kasper de Souza <thiagokasper101@gmail.com>
2022-11-12 15:03:19 -0300

    #1-release: implementando struct de produto

--- main.go
+++ main.go
@@ -8,6 +8,13 @@ import (
 
 var templates = template.Must(template.ParseGlob("templates/*.html"))
 
+type Produto struct {
+	Nome 		string
+	Descricao 	string
+	Preco 		float64
+	Quantidade 	int
+}
+
 func main() {
 	fmt.Println("Serving on http://localhost:8000")
 	getRoutes()
@@ -19,5 +26,9 @@ func getRoutes() {
 }
 
 func index(w http.ResponseWriter, r *http.Request ) {
-	templates.ExecuteTemplate(w, "Index", nil )
+	produtos := []Produto{
+		{ Nome:"Camiseta", Descricao:"Teste 123", Preco: 39,Quantidade: 5 },
+		{ Nome:"Tênis", Descricao:"Teste 123", Preco: 299, Quantidade: 3},
+	}
+	templates.ExecuteTemplate(w, "Index", produtos)
 }
